8ce4a6aa938be9015a975ef95f8daa7043d38501
Pranabemed <pranab.behera@emedevents.com>
2026-01-07 18:16:42 +0530

    first commit

---
+++ ios/SpeechRecognition.swift
@@ -0,0 +1,8 @@
+//
+//  SpeechRecognition.swift
+//  eMedEvents
+//
+//  Created by eMed Events on 07/01/26.
+//
+
+import Foundation
---
+++ ios/eMedEvents-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  eMedEvents-Bridging-Header.h
+//  eMedEvents
+//
+//  Created by eMed Events on 07/01/26.
+//
+
+#ifndef eMedEvents_Bridging_Header_h
+#define eMedEvents_Bridging_Header_h
+
+
+#endif /* eMedEvents_Bridging_Header_h */
